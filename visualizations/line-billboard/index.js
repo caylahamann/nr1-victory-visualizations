d3fa40287b000c95282e4a0c1afb6e356cdfb8be
Cayla Hamann <chamann@newrelic.com>
2021-11-12 10:25:19 -0500

    Nerd days demo code

--- visualizations/line-billboard/index.js
+++ visualizations/line-billboard/index.js
@@ -10,6 +10,10 @@ import {
 } from 'nr1';
 import NrqlQueryError from '../../src/nrql-query-error';
 import NoDataState from '../../src/no-data-state';
+import { VictoryLine, VictoryLabel } from 'victory';
+import { baseLabelStyles } from '../../src/theme';
+import { formatNumberTicks } from '../../src/utils/units';
+import { linearRegression } from 'simple-statistics';
 
 export default class BillboardLineChart extends React.Component {
   static propTypes = {
@@ -17,10 +21,51 @@ export default class BillboardLineChart extends React.Component {
       accountId: PropTypes.number,
       query: PropTypes.string,
     }),
+    showColorForTrend: PropTypes.bool,
+  };
+
+  nrqlInputIsValid = (rawData) => {
+    const { metadata } = rawData[0];
+
+    return (
+      metadata.groups.length === 1 &&
+      metadata.groups[0]?.type === 'function' &&
+      metadata.groups[0]?.value === 'count'
+    );
+  };
+
+  transformData = (rawData) => {
+    const { data } = rawData[0];
+    let total = 0;
+
+    const lineData = data.map(({ x, y }) => {
+      total = total + y;
+      return { x, y };
+    });
+
+    return { lineData, total };
+  };
+
+  getColor = (rawData) => {
+    const { data } = rawData[0];
+
+    const dataArray = data.map(({ x, y }) => [x, y]);
+
+    const { m } = linearRegression(dataArray);
+
+    if (m > 0) {
+      return 'green';
+    }
+
+    if (m < 0) {
+      return 'red';
+    }
+
+    return 'grey';
   };
 
   render() {
-    const { nrqlQuery } = this.props;
+    const { nrqlQuery, showColorForTrend } = this.props;
 
     const nrqlQueryPropsAvailable =
       nrqlQuery && nrqlQuery.accountId && nrqlQuery.query;
@@ -55,11 +100,51 @@ export default class BillboardLineChart extends React.Component {
                 return <NoDataState />;
               }
 
+              if (!this.nrqlInputIsValid(data)) {
+                return (
+                  <NrqlQueryError title="NRQL Syntax Error" description="" />
+                );
+              }
+
+              console.log(data);
+
+              const { lineData, total } = this.transformData(data);
+
               return (
-                <div width={width} height={height}>
-                  <HeadingText>{nrqlQuery.accountId}</HeadingText>
-                  <HeadingText>{nrqlQuery.query}</HeadingText>
-                </div>
+                <svg
+                  width={width}
+                  height={height}
+                  viewBox={`0 0 ${width} ${height}`}
+                >
+                  <VictoryLine
+                    standalone={false}
+                    width={width}
+                    height={height}
+                    data={lineData}
+                    style={{
+                      data: {
+                        opacity: 0.15,
+                        strokeWidth: 4,
+                        stroke: showColorForTrend
+                          ? this.getColor(data)
+                          : 'blue',
+                      },
+                    }}
+                  />
+                  <VictoryLabel
+                    text={[
+                      formatNumberTicks({ unitType: 'COUNT', tick: total }),
+                      data[0].metadata.name,
+                    ]}
+                    x={width / 2}
+                    y={height / 2}
+                    textAnchor="middle"
+                    style={[
+                      { ...baseLabelStyles, fontSize: 88, fill: '#2a3434' },
+                      { ...baseLabelStyles, fontSize: 24 },
+                    ]}
+                  />
+                </svg>
               );
             }}
           </NrqlQuery>
